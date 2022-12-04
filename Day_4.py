42bfef374cd5b2a677f24d09b3a92e9ad18c4807
Bethany Marshall <82471699+BethDataHub@users.noreply.github.com>
2022-12-04 20:13:02 +0000

    Update Day_4.py

--- Day_4.py
+++ Day_4.py
@@ -15,28 +15,22 @@ def get_answers(df):
     df["between"] = (df["Second Start"].between(df["First Start"],df["First End"]) & df["Second End"].between(df["First Start"],df["First End"])) | (df["First Start"].between(df["Second Start"],df["Second End"]) & df["First End"].between(df["Second Start"],df["Second End"]))
 
     df["overlap"] = df["Second Start"].between(df["First Start"],df["First End"]) | df["Second End"].between(df["First Start"],df["First End"]) | df["First Start"].between(df["Second Start"],df["Second End"]) | df["First End"].between(df["Second Start"],df["Second End"])
-
-    df["test"] = df["Second Start","Second End"].between(df["First Start"],df["First End"])
     
-    #part_one = df[df["between"]==True].count()["between"]
-    #part_two = df[df["overlap"]==True].count()["between"]
+    part_one = df[df["between"]==True].count()["between"]
+    part_two = df[df["overlap"]==True].count()["between"]
 
-    print(df)
+    return part_one, part_two
 
-    #return part_one, part_two
+def main():
+    test_part_one,test_part_two = get_answers(get_challenge_data(day=day, column_name=["First Elf","Second Elf"]))
+    part_one,part_two = get_answers(get_challenge_data(day=day, column_name=["First Elf","Second Elf"], run_type="actual"))
 
-get_answers(df)
+    if part_two and part_one:
+        test_before_submit(day, part="b", test_answer=test_part_two, answer=part_two)
+    elif part_one:
+        test_before_submit(day, part="a", test_answer= test_part_one, answer=part_one)
+    else:
+        raise ValueError("Missing Answers")
 
-#def main():
-#    test_part_one,test_part_two = get_answers(get_challenge_data(day=day, column_name=["First Elf","Second Elf"]))
-#    part_one,part_two = get_answers(get_challenge_data(day=day, column_name=["First Elf","Second Elf"], run_type="actual"))
-#
-#    if part_two and part_one:
-#        test_before_submit(day, part="b", test_answer=test_part_two, answer=part_two)
-#    elif part_one:
-#        test_before_submit(day, part="a", test_answer= test_part_one, answer=part_one)
-#    else:
-#        raise ValueError("Missing Answers")
-#
-#if __name__ == "__main__":
-#    main()
+if __name__ == "__main__":
+    main()
